164347cebd1a2385767c6bc266f211a16651ee69
Eric Lozaga <abda53@gmail.com>
2026-01-06 03:04:45 -0800

    latest

--- assets/build/js/custom.js
+++
@@ -1,1 +0,0 @@
-!function($){"use strict";$(window).load((function(){$(".preloader").fadeOut(1e3)})),$(".owl-carousel").owlCarousel({animateOut:"fadeOut",items:1,loop:!0,autoplayHoverPause:!1,autoplay:!0,smartSpeed:2e3}),$.stellar({horizontalScrolling:!1}),new WOW({mobile:!1}).init()}(jQuery);
